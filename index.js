be34672515ffa6d64c3518538fa1866d560ed283
Patricio López <lopezjuripatricio@gmail.com>
2016-06-11 18:59:21 -0400

    add socket.io integration

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 'use strict';
 
 const five = require('johnny-five');
+const io = require('socket.io-client');
+
+const host = 'http://localhost:5000';
 const board = new five.Board();
 
 board.on('ready', function() {
   const led = new five.Led(13);
-  led.blink(500);
+
+  const socket = io.connect(host);
+
+  socket.on('connect', function() {
+    console.log('Connected');
+    led.blink(500);
+  });
 });
